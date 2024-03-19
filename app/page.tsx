2254538109321bb2abf11d6f63f961fc86d941f6
Muhammad Hafizuddin <100256725+apiz23@users.noreply.github.com>
2024-03-20 01:32:24 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -9,16 +9,9 @@ export default function Home() {
 	const [isLoading, setIsLoading] = useState(true);
 
 	useEffect(() => {
-		(async () => {
-			const LocomotiveScroll = (await import("locomotive-scroll")).default;
-			const locomotiveScroll = new LocomotiveScroll();
-
-			setTimeout(() => {
-				setIsLoading(false);
-				document.body.style.cursor = "default";
-				window.scrollTo(0, 0);
-			}, 2000);
-		})();
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 2000);
 	}, []);
 
 	return (

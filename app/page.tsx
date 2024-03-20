3305e541b2cb385332648006f7c1b42612dec9c2
Hafizu <piz230601@gmail.com>
2024-03-20 09:57:33 +0800

    add locomotive

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ import Preloader from "@/components/Preloader";
 import { AnimatePresence } from "framer-motion";
 import { useEffect, useState } from "react";
 import Landing from "./landing/page";
+import LocomotiveScroll from "locomotive-scroll";
 
 export default function Home() {
 	const [isLoading, setIsLoading] = useState(true);
+	const locomotiveScroll = new LocomotiveScroll();
 
 	useEffect(() => {
 		setTimeout(() => {
@@ -19,15 +21,22 @@ export default function Home() {
 			<main className="min-h-screen bg-white dark:bg-black">
 				<AnimatePresence mode="wait">{isLoading && <Preloader />}</AnimatePresence>
 				{/* <Landing /> */}
-				<div className="text-center px-10 pt-28">
-					<h1 className="scroll-m-20 md:text-7xl mc-auto text-4xl font-extrabold tracking-tight">
+				<div className="text-center px-10 pt-28 h-[50vh]">
+					<h1
+						className="scroll-m-20 md:text-7xl mc-auto text-4xl font-extrabold tracking-tight"
+						data-scroll
+						data-scroll-speed="0.5"
+					>
 						Welcome to my blog
 					</h1>
-					<p className="leading-7 px-10 [&:not(:first-child)]:mt-6 text-zinc-400">
+					<p
+						className="leading-7 px-10 [&:not(:first-child)]:mt-6 text-zinc-400"
+						data-scroll
+						data-scroll-speed="0.7"
+					>
 						One of the source of learning material for my Studies Journey
 					</p>
 				</div>
-				
 			</main>
 		</>
 	);

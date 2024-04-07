f2812b8ea2d89504ae294dc0744ceb629d6e66ce
Hafizu <piz230601@gmail.com>
2024-04-08 00:20:51 +0800

    change api to test on production

--- app/api/link/route.ts
+++ app/api/link/route.ts
@@ -1,18 +1,18 @@
 "use server";
 
 import supabase from "@/lib/supabase";
+import { NextApiResponse } from "next";
 import { NextResponse } from "next/server";
 
-export async function GET() {
+export async function GET(req: Request, context: any, res: NextApiResponse) {
 	try {
 		const { data, error } = await supabase.from("link").select("*");
-
 		if (error) {
 			throw error;
 		}
 
 		return NextResponse.json(data);
 	} catch (error: any) {
-		console.error("Error fetching data from Supabase:", error.message);
+		return res.status(500).json({ error: error.message });
 	}
 }

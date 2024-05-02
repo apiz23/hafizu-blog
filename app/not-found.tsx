eea12d51b0e377755d606b77c57c2206379c2205
Hafizu <piz230601@gmail.com>
2024-05-02 18:05:53 +0800

    fix syntax

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -38,7 +38,7 @@ export default function NotFound() {
 					<span className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-yellow-300 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
 
 					<span className="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold uppercase tracking-widest text-black group-active:text-opacity-75">
-						{countdown > 0 ? `Redirect in ${countdown} seconds` : "Let's Gooo"}
+						{countdown > 0 ? `Redirect in ${countdown} seconds` : "Lets Gooo"}
 					</span>
 				</button>
 			</div>

34af4c0a353812d321700c13b8b52e959c9ada98
Hafizu <piz230601@gmail.com>
2024-05-02 18:10:04 +0800

    fix again syntax

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -29,7 +29,7 @@ export default function NotFound() {
 				<h1 className="mt-6 text-2xl font-bold tracking-tight text-gray-900 sm:text-4xl">
 					Nuh Uh!
 				</h1>
-				<p className="my-4 text-gray-500">We can't find that page.</p>
+				<p className="my-4 text-gray-500">We can{"'"}t find that page.</p>
 
 				<button
 					className="group relative inline-block focus:outline-none focus:ring"
@@ -38,7 +38,7 @@ export default function NotFound() {
 					<span className="absolute inset-0 translate-x-1.5 translate-y-1.5 bg-yellow-300 transition-transform group-hover:translate-x-0 group-hover:translate-y-0"></span>
 
 					<span className="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold uppercase tracking-widest text-black group-active:text-opacity-75">
-						{countdown > 0 ? `Redirect in ${countdown} seconds` : "Lets Gooo"}
+						{countdown > 0 ? `Redirect in ${countdown} seconds` : "Let's Gooo"}
 					</span>
 				</button>
 			</div>

b44f5c25dd37503289b9afcd94639cd0bdd7c7e4
Hafizu <piz230601@gmail.com>
2024-08-24 21:01:44 +0800

    remove the callback

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -8,22 +8,6 @@ const handler = NextAuth({
 			clientSecret: process.env.GITHUB_SECRET as string,
 		}),
 	],
-	callbacks: {
-		async jwt({ token, account, profile }) {
-			if (account && profile) {
-				token.id = account.providerAccountId;
-				token.username = profile.login;
-			}
-			return token;
-		},
-		async session({ session, token }) {
-			if (token.id && token.username) {
-				session.user.id = token.id as string;
-				session.user.username = token.username as string;
-			}
-			return session;
-		},
-	},
 });
 
 export { handler as GET, handler as POST };
